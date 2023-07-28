a818173341c688a88e5b0e3128f94a56aa873d51
shen_zhifeng <shen_zhifeng@outlook.com>
2023-07-28 22:09:00 +0800

    修复google翻译问题

--- trans/web_trans.py
+++ trans/web_trans.py
@@ -83,7 +83,7 @@ class google(web_translator):
         res = strip_breaks(rawtext)
         res = strip_tags(res)
         self.inputArea.send_keys(res)
-        xpath = '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[2]/div/div[8]/div/div[1]/span[1]'
+        xpath = '/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div/div[2]/div[3]/c-wiz[2]/div/div[9]/div/div[1]'
         success = False
         try:
             time.sleep(random.uniform(0, 1))  # 设置随机等待时间，防止触发反bot机制
