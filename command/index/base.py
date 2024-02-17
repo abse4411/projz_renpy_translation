bf127bbe2e86a12eb2d063a51bd151f386db9604
abse4411 <shen_zhifeng@outlook.com>
2024-02-17 22:04:14 +0800

    减少ls命令显示的信息

--- command/index/base.py
+++ command/index/base.py
@@ -27,6 +27,7 @@ from util import yes, quick_prettytable
 class ListTranslationIndexCmd(BaseCmd):
     def __init__(self):
         super().__init__('ls', 'List existing TranslationIndexes.')
+        self._parser.add_argument("-a", "--all", action='store_true', help="List all information about the game.")
 
     def invoke(self):
         res = TranslationIndex.list_indexes()
@@ -55,8 +56,11 @@ class ListTranslationIndexCmd(BaseCmd):
             injection_table = quick_prettytable([[k, v] for k, v in index.injection_state.items()])
             injection_table.vrules = prettytable.NONE
             injection_table.hrules = prettytable.NONE
-            game_info = (f'{index.project_name} ({index.project_version})\n{index.project_renpy_version}\n'
-                         f'{index.project_path}')
+            if self.args.all:
+                game_info = (f'{index.project_name} ({index.project_version})\n{index.project_renpy_version}\n'
+                             f'{index.project_path}')
+            else:
+                game_info = f'{index.project_name} ({index.project_version})'
             table.add_row([i[0], f'{index.nickname}:{index.tag}', format_stats(index.translation_state),
                            injection_table, game_info])
         print('Note that: Translation Stats list translated/untranslated lines '
